781fa7bcb2ef4db159659d32eb607c57a3734ab6
Pedro H <pedrohenriquedasilva20@gmail.com>
2022-03-17 22:15:08 -0300

    Teste de tabela

--- src/com/ucs/formularios/FormCadConsulta.java
+++ src/com/ucs/formularios/FormCadConsulta.java
@@ -1,8 +1,11 @@
 package com.ucs.formularios;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.*;
+
+import static com.ucs.dados.ListaDeMedico._listaMedico;
 
 public class FormCadConsulta extends JDialog {
     private JPanel contentPane;
@@ -10,6 +13,7 @@ public class FormCadConsulta extends JDialog {
     private JButton btnCancelCadConsulta;
     private JTextField textField1;
     private JButton btnBuscarMedico;
+    private JTable table1;
 
     public FormCadConsulta() {
         setContentPane(contentPane);
@@ -21,6 +25,21 @@ public class FormCadConsulta extends JDialog {
                 dispose();
             }
         });
+        btnBuscarMedico.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        table1.addComponentListener(new ComponentAdapter() {
+        });
+        table1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+
     }
 
     public static void main(String[] args) {
@@ -28,5 +47,18 @@ public class FormCadConsulta extends JDialog {
         dialog.pack();
         dialog.setVisible(true);
         System.exit(0);
+    }
+
+    //link di video do youtube
+    //https://www.youtube.com/watch?v=3R1itvzQKpk
+    String header[] = {"Nome do medico", "Especialidade"};
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        DefaultTableModel model = new DefaultTableModel(0,2);
+        model.setColumnIdentifiers(header);
+        table1 = new JTable(model);
+        model.addRow(_listaMedico.toArray());
+
+
     }
 }
